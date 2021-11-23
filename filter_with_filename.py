ff6ee053556577275c56e26c1fa03921cddbfa9c
Helloeverybody <seva.bocharov@mail.ru>
2021-11-23 21:46:14 +0500

    выполнил 11 пункт

--- filter_with_filename.py
+++ filter_with_filename.py
@@ -3,6 +3,28 @@ import numpy as np
 
 
 def convert_image_to_mosaic(image, size, gradation_step):
+    '''
+    Переводит картинку в пиксельную мозаику
+    :param image: [[[int, int, int]]]
+    :param size: int
+    :param gradation_step: int
+
+    :return: [[[int, int, int]]]
+
+    >>> convert_image_to_mosaic((np.ones((3, 3, 3)) * 200), 2, 15)
+    array([[[195., 195., 195.],
+            [195., 195., 195.],
+            [ 90.,  90.,  90.]],
+    <BLANKLINE>
+           [[195., 195., 195.],
+            [195., 195., 195.],
+            [ 90.,  90.,  90.]],
+    <BLANKLINE>
+           [[ 90.,  90.,  90.],
+            [ 90.,  90.,  90.],
+            [ 45.,  45.,  45.]]])
+
+    '''
     for x in range(0, len(image), size):
         for y in range(0, len(image[0]), size):
             image[x:x + size, y:y + size] = get_average_brightness(
@@ -11,11 +33,32 @@ def convert_image_to_mosaic(image, size, gradation_step):
 
 
 def get_average_brightness(block, size, gradation_step):
+    '''
+    Берет усередненный серый цвет области
+    :param block: [[[int, int, int]]]
+    :param size: int
+    :param gradation_step: int
+    :return int
+
+    >>> get_average_brightness(np.ones((3, 3, 3)) * 200, 2, 15)
+    195
+    >>> get_average_brightness(np.ones((3, 3, 3)) * 100, 2, 15)
+    90
+    >>> get_average_brightness(np.ones((3, 3, 3)) * 100, 6, 6)
+    24
+    >>> get_average_brightness(np.ones((10, 10, 3)) * 100, 6, 6)
+    96
+    '''
     average_color = (block[:size, :size].sum() / 3) // size ** 2
-    return int(average_color // gradation_step) * gradation_step
+    a = int(average_color // gradation_step) * gradation_step
+    return a
 
 
 def main():
+    '''
+    Загружает картинку, отправляет ее в метод пикселизации,
+    а затем сохраняет полученный результат в файл
+    '''
     image_file = Image.open("scale1200.jpg")
     block_size = 10
     gradations_count = 50
